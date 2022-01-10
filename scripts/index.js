dcd2d6e7104669f8ed4f9256e13232fe9e4ecd8b
Leotheluck <leo.largillet@hetic.net>
2022-01-10 06:56:13 +0100

    Adding the files

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+const scooter = document.querySelector('.main .landing .center .background-image')
+
+setTimeout(() => {
+    scooter.style.transform = "translateX(0vw)"    
+    scooter.style.opacity = "1"
+}, 50);
+
+const wheel = document.querySelector('.main .landing .center .background-imagen')
+
+setTimeout(() => {
+    wheel.style.transform = "translateX(30vw)"    
+    wheel.style.opacity = "1"
+}, 50);
